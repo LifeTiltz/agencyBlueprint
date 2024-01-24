be3c128b135d5061c37877c0dccc4628ad069083
LifeTilts <85487129+LifeTiltz@users.noreply.github.com>
2024-01-24 17:49:36 +0000

    firstCommitBestCommit

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("yeahh boii");
+// import iFrameResize from "./node_modules/iframe-resizer/js/iframeResizer";
+const iFrameResize = require("./node_modules/iframe-resizer/js/iframeResizer");
+
+const iframes = iFrameResize({ log: true }, "first_video");
